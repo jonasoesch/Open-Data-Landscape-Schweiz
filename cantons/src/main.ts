825b2d28afee260e983038785fcb3f299cfe7047
Jonas Oesch <jonas.oesch@fhnw.ch>
2017-07-26 15:44:25 +0200

    Open Data Landkarte erste Version

--- cantons/src/main.ts
+++ cantons/src/main.ts
@@ -3,22 +3,19 @@ import * as topojson from 'topojson';
 
 
 
-var width = 960,
-    height = 500;
+var svg = d3.select('#map svg');
+//var colorscheme = ['#FFFF9D', '#BEEB9F', '#79BD8F', '#00A388'];
+let colorscheme = ['#D9CF99', '#9FBF8F', '#81A68A', '#579E9C'];
 
-var svg = d3.select('#swiss');
-
-var colors = {"Nein": '#FDFDFF', "Teilweise": '#C6C5B9', "Ja": "#62929E"};
-var colors2 = [ "#f3ffbd", "#b2dbbf", "#70c1b3", "#247ba0"];
+//var colors = {"Nein": '#FDFDFF', "Teilweise": '#C6C5B9', "Ja": "#62929E"};
+//var colors2 = [ "#f3ffbd", "#b2dbbf", "#70c1b3", "#247ba0"];
 
 d3.json('cantons.data.json', function(err, cantons){
 
     opendata();
 
-    svg.selectAll('#lakes *')
-        .attr('fill', "#546A7B");
 
-    svg.selectAll("#borders").attr('stroke', '#444545')
+    svg.selectAll("#borders").attr('stroke', '#444545');
 
     document.getElementById('opendata').addEventListener('click', function(){opendata()});
     document.getElementById('og').addEventListener('click', function(){oeffentlichkeitsgesetz()});
@@ -26,17 +23,26 @@ d3.json('cantons.data.json', function(err, cantons){
 
 
     function oeffentlichkeitsgesetz () {
-        svg.selectAll('#cantons g:first-child *')//.call((function(g) {console.log(g)} as any));
-            .attr('fill', function(canton) {return colors[cantons[this.id]['öffentlichkeitsgesetz']]} as any);
+        svg.selectAll('#cantons polygon, #cantons path')//.call((function(g) {console.log(g)} as any));
+            .attr('fill', function(canton) {console.log(this.id); return colorscheme[cantons['cantons'][this.id]['öffentlichkeitsgesetz']]} as any);
 
-        legend(Object.keys(colors).map(key => colors[key]), ['Kein Öffentlichkeitsprinzip', ' Evaluation', 'In Vorbereitung', 'Publikation']);
+        svg.selectAll('#cities, #city-labels').attr('class', 'hidden');
+
+        legend(colorscheme, ['Keine Information', 'Nein', 'Teilweise', 'Ja']);
+
+
     }
 
     function opendata() {
-        svg.selectAll("#cantons g:first-child *")
-            .attr('fill', function() {return colors2[cantons[this.id]['opendata']]} as any)
+        svg.selectAll("#cantons polygon, #cantons path")
+            .attr('fill', function() { return colorscheme[cantons['cantons'][this.id]['opendata']]} as any);
 
-        legend(colors2, ['Keine Angaben', 'In Evaluation', 'In Vorbereitung', 'Publikation']);
+        svg.selectAll("#cities polygon")
+            .attr('fill', function() {  return colorscheme[cantons['cities'][this.id]['opendata']]} as any);
+
+        svg.selectAll('#cities, #city-labels').attr('class', '');
+
+        legend(colorscheme, ['Keine Information', 'In Evaluation', 'In Vorbereitung', 'Publikation']);
     }
 });
 
@@ -51,7 +57,7 @@ function legend(colors:string[], texts:string[]) {
             .attr('class', 'legend');
 
         function legendPosition ( i:number, shift:number=0 ):string {
-            return String(i*4+85+shift) + '%';
+            return String(i*18+15+shift) + 'px';
         }
 
         legend.selectAll('rect')
@@ -60,7 +66,7 @@ function legend(colors:string[], texts:string[]) {
             .append('rect')
             .attr('width', 10)
             .attr('height', 10)
-            .attr('x', '76%')
+            .attr('x', '0px')
             .attr('y', (d, i) => legendPosition(i))
             .attr('fill', function(d):string{return d});
 
@@ -69,8 +75,8 @@ function legend(colors:string[], texts:string[]) {
             .data(colors)
             .enter()
             .append('text')
-            .attr('x', '79%')
-            .attr('y', (d, i) => legendPosition(i, 2))
+            .attr('x', '20px')
+            .attr('y', (d, i) => legendPosition(i, 10))
             .attr('fill', '#333')
             .attr('font-family', 'sans-serif')
             .attr('font-size', '0.8em')
