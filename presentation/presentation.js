e07d1b3a585333bd47585b8e40f512ff7be55265
Jonas Oesch <jonas.oesch@fhnw.ch>
2017-07-28 09:25:26 +0200

    We also need the styling files

---
+++ presentation/presentation.js
@@ -0,0 +1,5 @@
+var css = document.createElement('link');
+css.setAttribute('rel', 'stylesheet')
+css.setAttribute('type', 'text/css')
+css.setAttribute('href', 'presentation/styles.css')
+document.getElementsByTagName('head')[0].appendChild(css);
