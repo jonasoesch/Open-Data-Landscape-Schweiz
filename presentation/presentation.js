0348e21016dc9cc3394c0bb29b86e73c7662d57b
Jonas Oesch <jonas.oesch@fhnw.ch>
2017-07-28 11:14:49 +0200

    Added cantons map via iframe

--- presentation/presentation.js
+++ presentation/presentation.js
@@ -1,5 +1,40 @@
+var head = document.getElementsByTagName('head')[0];
 var css = document.createElement('link');
 css.setAttribute('rel', 'stylesheet')
 css.setAttribute('type', 'text/css')
 css.setAttribute('href', 'presentation/styles.css')
-document.getElementsByTagName('head')[0].appendChild(css);
+head.appendChild(css);
+
+
+/*
+var landscape_script = document.createElement('script');
+landscape_script.setAttribute('src', 'cantons/dist/main.js')
+head.appendChild(landscape_script)
+
+
+ajax('cantons/dist/index.html', function(resp){
+    var landscape = document.getElementById('landscape');
+    landscape.innerHTML = resp;
+});
+
+
+function ajax(url, callback) {
+    var request = new XMLHttpRequest();
+    request.open('GET', url, true);
+    request.onload = function() {
+      if (request.status >= 200 && request.status < 400) {
+        callback(request.responseText);
+      } else {
+          callback("")
+      }
+    };
+
+    request.onerror = function() {
+        callback("");
+    };
+
+    request.send();
+
+}
+
+*/
